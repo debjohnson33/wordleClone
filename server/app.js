87b50eae761f38c03e58943a8e6880bddc71d41b
Deb Johnson <friendswithdrj@gmail.com>
2022-02-04 15:27:45 -0600

    Add webpack/babel/react

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public/dist'));
 
 app.listen(PORT, () => {
   console.log(`Wordle Clone App listening on port ${PORT}!`);
