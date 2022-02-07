5f48fb6296708b509250aae9637acfd73b9d238c
Deb Johnson <friendswithdrj@gmail.com>
2022-02-07 17:32:45 -0600

    Add get all words endpoint to api

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
+const exp = require('constants');
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+const db = require('../db/pool.js');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public/dist'));
+
+app.get('/api/words/all', (req, res) => {
+  db.getAllWords((err, results) => {
+    console.log(`Getting all words`);
+    err ? console.log(err) : res.send(results);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Wordle Clone App listening on port ${PORT}!`);
