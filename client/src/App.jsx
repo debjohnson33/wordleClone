46bcd18ac2d7c4894cac449ce97088ba8fc598a2
Deb Johnson <friendswithdrj@gmail.com>
2022-02-08 17:02:38 -0600

    Add hook to retrieve random word/set state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [currentWord, setWord] = useState("");
+
+  useEffect(() => {
+    const result = axios.get('/api/getRandomWord')
+      .then(res => {
+        console.log(res);
+        setWord(res.data[0].word);
+      })
+  }, [""]);
+
   return (
     <div>
-      <h1>Wordle Clone!</h1>
+      <h1>{currentWord}</h1>
     </div>
   );
 }
