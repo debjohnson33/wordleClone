4d410ac1ccda63ac51c2fc850f2e500f2a2f50fe
Deb Johnson <friendswithdrj@gmail.com>
2022-02-10 17:29:42 -0600

    Switch to using letter tile component

--- client/src/components/Alphabet.jsx
+++ client/src/components/Alphabet.jsx
@@ -1,40 +1,25 @@
 import React from "react";
 
 import styles from "../styles.css";
+import LetterTile from "./LetterTile.jsx";
 
-const Alphabet = () => {
+const Alphabet = ({greenLetters, yellowLetters, blackLetters}) => {
+  // logic to take letters and change the background color
+  // for each green letter, change background to green
+  // for each yellow letter, change background to yellow
+  // for each black letter, change background to black
+
+  // Make letter tile component and pass in letter, background color
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const className = styles.letter;
   return (
     <div>
       <h1>Available letters:</h1>
       <div className={styles.container}>
-        <div className={styles.letter} id="A">A</div>
-        <div className={styles.letter} id="B">B</div>
-        <div className={styles.letter} id="C">C</div>
-        <div className={styles.letter} id="D">D</div>
-        <div className={styles.letter} id="E">E</div>
-        <div className={styles.letter} id="F">F</div>
-        <div className={styles.letter} id="G">G</div>
-        <div className={styles.letter} id="H">H</div>
-        <div className={styles.letter} id="I">I</div>
-        <div className={styles.letter} id="J">J</div>
-        <div className={styles.letter} id="K">K</div>
-        <div className={styles.letter} id="L">L</div>
-        <div className={styles.letter} id="M">M</div>
-        <div className={styles.letter} id="N">N</div>
-        <div className={styles.letter} id="O">O</div>
-        <div className={styles.letter} id="P">P</div>
-        <div className={styles.letter} id="Q">Q</div>
-        <div className={styles.letter} id="R">R</div>
-        <div className={styles.letter} id="S">S</div>
-        <div className={styles.letter} id="T">T</div>
-        <div className={styles.letter} id="U">U</div>
-        <div className={styles.letter} id="V">V</div>
-        <div className={styles.letter} id="W">W</div>
-        <div className={styles.letter} id="X">X</div>
-        <div className={styles.letter} id="Y">Y</div>
-        <div className={styles.letter} id="Z">Z</div>
+        {alphabet.map((letter, i) => (
+          <LetterTile className={className} letter={letter} bgColor={greenLetters.includes(letter) ? "green" : yellowLetters.includes(letter) ? "yellow" : blackLetters.includes(letter) ? "black" : "white"} i={i} />
+        ))}
       </div>
-
     </div>
   )
 };
