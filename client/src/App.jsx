cb4589bbfa3f782619a109fb8293d74c256e7eee
Deb Johnson <friendswithdrj@gmail.com>
2022-02-10 17:34:30 -0600

    Add to state/edit props/add guesses disp component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,20 @@ import axios from "axios";
 
 import WordGuessForm from "./components/WordGuessForm.jsx";
 import Alphabet from "./components/Alphabet.jsx";
+import GuessesDisplay from "./components/GuessesDisplay.jsx";
 import styles from "./styles.css";
 
 const App = () => {
   const [currentWord, setWord] = useState("");
+  // Array of green letters - pass set function to WordGuessForm
+  //  then pass array to Alphabet
+  const [greenLetters, setGreenLetters] = useState([]);
+  // Array of yellow letters
+  const [yellowLetters, setYellowLetters] = useState([]);
+  // Array of black letters
+  const [blackLetters, setBlackLetters] = useState([]);
+  const [guess, setGuess] = useState("");
+  const [guesses, setGuesses] = useState([]);
 
   useEffect(() => {
     axios.get('/api/getRandomWord')
@@ -19,8 +29,9 @@ const App = () => {
   return (
     <div>
       <h1>{currentWord}</h1>
-      <Alphabet />
-      <WordGuessForm currentWord={currentWord} />
+      <Alphabet greenLetters={greenLetters} yellowLetters={yellowLetters} blackLetters={blackLetters} />
+      <WordGuessForm guess={guess} currentWord={currentWord} setGreenLetters={setGreenLetters} setYellowLetters={setYellowLetters} setBlackLetters={setBlackLetters} setGuess={setGuess} setGuesses={setGuesses} />
+      <GuessesDisplay guesses={guesses} greenLetters={greenLetters} yellowLetters={yellowLetters} blackLetters={blackLetters} />
     </div>
   );
 }
