850f2965908764b9068be12681e7570cc690f4b2
Deb Johnson <friendswithdrj@gmail.com>
2022-02-11 17:38:52 -0600

    Add color to Tile component

--- client/src/components/LetterTile.jsx
+++ client/src/components/LetterTile.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 import styles from "../styles.css";
 
-const LetterTile = ({ className, letter, bgColor, i }) => {
+const LetterTile = ({ className, letter, bgColor, i, color }) => {
   return (
-    <div className={className} style={{ backgroundColor: bgColor}} key={i} >{letter}</div>
+    <div className={className} style={{ backgroundColor: bgColor, color: color}} key={i} >{letter}</div>
   )
 };
 
