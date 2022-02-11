9c689f729ab216451fc8eb43cb9142c2eea4abf7
Deb Johnson <friendswithdrj@gmail.com>
2022-02-11 17:40:13 -0600

    Change to div vs component temporarily

--- client/src/components/WordDisplay.jsx
+++ client/src/components/WordDisplay.jsx
@@ -9,7 +9,9 @@ const WordDisplay = ({ guess, greenLetters, yellowLetters, blackLetters, index }
   return (
     <div className={styles.wordcontainer} key={index}>
       {guess.split("").map((letter, i) => (
-        <LetterTile className={className} letter={letter} bgColor={greenLetters.includes(letter) ? "green" : yellowLetters.includes(letter) ? "yellow" : blackLetters.includes(letter) ? "black" : "white"} i={i + 300} />
+        <div className={className} style={{backgroundColor: "white"}} key={i} >
+          {letter}
+        </div>
       ))}
     </div>
   );
