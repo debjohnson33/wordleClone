5ed4e51b7204b1a24d64430da150e125e8c01a1f
Deb Johnson <friendswithdrj@gmail.com>
2022-02-24 16:20:46 -0600

    Change to close button

--- client/src/LoserModal.jsx
+++ client/src/LoserModal.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styles from "./styles.css";
 
-const LoserModal = ({ correctWord }) => {
+const LoserModal = ({ correctWord, onQuit }) => {
   return (
     <div className={styles.modal}>
       <h2>Better Luck Next Time!</h2>
       <h3>{correctWord}</h3>
+      <button onClick={onQuit}>Close</button>
     </div>
   )
 };
--- client/src/WinnerModal.jsx
+++ client/src/WinnerModal.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styles from "./styles.css";
 
-const WinnerModal = ({ numGuesses }) => {
+const WinnerModal = ({ numGuesses, onQuit }) => {
   return (
     <div className={styles.modal}>
       <h2>WINNER!</h2>
       <p>You guessed the word in {numGuesses} guesses!</p>
+      <button onClick={onQuit}>Close</button>
     </div>
   )
 };
