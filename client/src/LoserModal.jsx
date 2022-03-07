562f15359769b2996575859f3fdae79ad93415ee
Deb Johnson <friendswithdrj@gmail.com>
2022-03-07 15:52:56 -0600

    Add div for more style/fix typo

--- client/src/LoserModal.jsx
+++ client/src/LoserModal.jsx
@@ -5,15 +5,17 @@ import styles from "./styles.css";
 const LoserModal = ({ correctWord, onQuit }) => {
   return (
     <div className={styles.modal}>
-      <h2>Better Luck Next Time!</h2>
-      <h3>{correctWord}</h3>
-      <button onClick={onQuit}>Close</button>
+      <div id={styles.loser}>
+        <h2>Better Luck Next Time!</h2>
+        <h3>The word was {correctWord}</h3>
+        <button onClick={onQuit}>Close</button>
+      </div>
     </div>
   )
 };
 
 const Modal = (props) => {
-  return (ReactDOM.createPortal(LoserModal, document.getElementById("#LoserModal")));
+  return (ReactDOM.createPortal(LoserModal, document.getElementById("#loserModal")));
 }
 
 export default LoserModal;
--- client/src/WinnerModal.jsx
+++ client/src/WinnerModal.jsx
@@ -5,15 +5,17 @@ import styles from "./styles.css";
 const WinnerModal = ({ numGuesses, onQuit }) => {
   return (
     <div className={styles.modal}>
-      <h2>WINNER!</h2>
-      <p>You guessed the word in {numGuesses} guesses!</p>
-      <button onClick={onQuit}>Close</button>
+      <div id={styles.winner}>
+        <h2>WINNER!</h2>
+        <p>You guessed the word in {numGuesses} guesses!</p>
+        <button onClick={onQuit}>Close</button>
+      </div>
     </div>
   )
 };
 
 const Modal = (props) => {
-  return (ReactDOM.createPortal(WinnerModal, document.getElementById("#WinnerModal")));
+  return (ReactDOM.createPortal(WinnerModal, document.getElementById("#winnerModal")));
 }
 
 export default WinnerModal;
